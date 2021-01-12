c2a79d48fc7d3b7a0e312e5b3fcccbdaef9f1ae8
Muhammad Ariful Islam <arifparvez@sharetrip.net>
2021-01-12 18:21:23 +0600

    Added SwiftUI ray course project.
    Added SwiftUI ray course project.

--- Learn Android With Kotlin/3.AppNavigation/TriviaApp/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Learn Android With Kotlin/3.AppNavigation/TriviaApp/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -26,7 +26,7 @@ import com.example.android.navigation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
+        //@Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
